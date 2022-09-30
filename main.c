b2a9cd5461aaf41cfd215d4ba92fa4445051df88
Bhargav Sai <114660786+bhargavsai-lingampalli@users.noreply.github.com>
2022-09-30 10:45:55 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main(void)
     int i,num,res,range,count=1;
     printf("enter number: ");
     scanf("%d",&range);
+    printf("2\t");
     for(num=3; num<range; num+=2)
     {
         for(i=3,res=0; i<num && res==0; i+=2)
