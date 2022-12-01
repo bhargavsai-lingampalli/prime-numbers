61b3af51697d5eedbf035d1fb11ca99b19eef330
Bhargav Sai <114660786+BhargavSai-Lingampalli@users.noreply.github.com>
2022-12-01 11:08:59 +0530

    Update main.c
    much faster

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 void main()
 {
-    int i,num,res,range,count=1,ofnum;
+    int i,num,res,range,count=1,sqnum;
     printf("enter number: ");
     scanf("%d",&range);
     printf("2\t");
     for(num=3; num<range; num+=2)
     {
-        ofnum=num/2;
-        for(i=3,res=0; i<ofnum && res==0; i+=2)
+        sqnum=pow(num,0.5);
+        for(i=3,res=0; i<sqnum && res==0; i+=2)
         {
             if(num%i==0)
                 res=1;
